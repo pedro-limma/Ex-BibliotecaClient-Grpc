05911a32a39a0b3567312cf1a5ebd8b02e6e922e
palima3 <palima3@STFC047247-L>
2022-05-24 00:39:14 -0300

    Adicionar arquivos de projeto.

---
+++ BibliotecaClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using BibliotecaClient;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7242");
+
+var client = new BibliotecaServiceGrpc.BibliotecaServiceGrpcClient(channel);
+
+//exemplos de requests
+//SelectAll  - await client.SelectAllAsync(new Empty());
+//SelectById - await client.SelectByIDAsync(new LivroFilter() { LivroID = 1 });
+//Insert     - await client.InsertAsync(new Livro() { LivroID = 3, Name = "As viagens de gulliver", Tipo= "Ficção"});
+//Update     - await client.UpdateAsync(new Livro() { LivroID = 1, Name = "Diario De um Banana", Tipo = "Comédia" });
+//Delete     - await client.DeleteAsync(new LivroFilter() { LivroID = 3});
+
+var reply = await client.SelectAllAsync(new Empty());
+
+
+
+Console.WriteLine(reply.ToString());
+//Console.WriteLine((await client.SelectAllAsync(new Empty())).ToString());
+Console.ReadLine();
